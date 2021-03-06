8219182bde6d599e77a160ec49674507aec4127a
Rakesh K. Cherukuri <rakesh@digital.ai>
2021-03-06 08:55:35 +0530

    including 135

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,26 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+}
